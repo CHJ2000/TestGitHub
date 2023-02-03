302ec942f74abccb3bda765a85f975e6334f0f61
Carter Jeffery <jeffcart@sheridancollege.ca>
2023-02-03 16:53:19 -0500

    changed the code back

--- src/testgithub/TestGitHub.java
+++ src/testgithub/TestGitHub.java
@@ -4,10 +4,17 @@
  */
 package testgithub;
 
+/**
+ *
+ * @author carte
+ */
 public class TestGitHub {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-       
+        // TODO code application logic here
     }
      
 }
