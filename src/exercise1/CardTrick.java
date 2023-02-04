cb878312d4ef62c5f2981604d636390865a00ffc
Carter Jeffery <jeffcart@sheridancollege.ca>
2023-02-04 12:47:21 -0500

    Added random class for Exercise 1

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,12 +1,14 @@
 package exercise1;
-
+import java.util.Random;
+import java.util.Scanner;
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
  *
  * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author Paul Bonenfant Jan 25, 2022
+ * @author Carter Jeffery Feb 3, 2023
  */
 public class CardTrick {
     
@@ -16,13 +18,16 @@ public class CardTrick {
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            Random rand = new Random();
+            card.setValue(rand.nextInt(14));
+            card.setSuit(Card.SUITS[rand.nextInt(4)]);
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
         }
 
+        Scanner input = new Scanner(System.in);
+        //System.out.println("Pick a card any card! (A card value between 1 and 13 and a suit)");
+        
         // insert code to ask the user for Card value and suit, create their card
         // and search the hand here. 
         // Hint: You can ask for values 1 to 10, and then
