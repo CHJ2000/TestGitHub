072310e4e6bb61ad1652d3c2e33b278bfab05c6a
Carter Jeffery <jeffcart@sheridancollege.ca>
2023-02-03 19:19:33 -0500

    First commit for GitHub

---
+++ src/testgithub/GitHub.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package testgithub;
+
+/**
+ *
+ * @author carte
+ */
+public class GitHub {
+    public static void main(String[] args) {
+        
+    }
+}
